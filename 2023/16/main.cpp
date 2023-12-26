16b839ec8244293d767939ded863649099f44687
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2023-12-27 00:56:25 +0200

    Fix 16

--- 2023/16/main.cpp
+++ 2023/16/main.cpp
@@ -95,6 +95,7 @@ static int64_t sCalculateEnergy(const char* data, int hashStart)
     static const int MAX_POSITIONS = 128;
     // Using visitedMap array vs hashset seems to be over 20x faster. 200ms -> 7ms
     alignas (16)uint8_t visitedMap[128 * 128]= {};
+    alignas (16)uint8_t visitedBoolMap[128 * 128 / 8]= {};
     // Using constant size stack allocated array saves also a bit of time compared to std::vector, although
     // if it was static it probably would not make a difference.
     int32_t positions[MAX_POSITIONS] = {};
@@ -122,6 +123,7 @@ static int64_t sCalculateEnergy(const char* data, int hashStart)
                 break;
             }
             visitedMap[index] |= dir;
+            visitedBoolMap[index / 8] |= (1 << (index % 8));
             char c = data[y * (width + 1) + x];
             switch(c)
             {
@@ -180,9 +182,16 @@ static int64_t sCalculateEnergy(const char* data, int hashStart)
     }
     {
         //TIMEDSCOPE("Calculate sum");
-#if 1 // Use simd for summing ~6ms -> ~4.8ms. Each iteration goes from ~4us to 2us
+#if 0
+        const uint64_t* ptr = (const uint64_t*) visitedBoolMap;
+        for(int i = 0; i < 128 * 128 / 8; ++i)
+        {
+            energy += std::popcount(*ptr);
+            ptr++;
+        }
+#elif 1 // Use simd for summing ~6ms -> ~4.8ms. Each iteration goes from ~4us to 2us
         __m128i ones8 = _mm_set1_epi8(1);
-        __m128i ones16 = _mm_set1_epi16(3);
+        __m128i ones16 = _mm_set1_epi16(7);
 
         const __m128i* ptr = (const __m128i*) visitedMap;
 
@@ -194,7 +203,10 @@ static int64_t sCalculateEnergy(const char* data, int hashStart)
             // then bitshift right by 1 and or them.
             // Then to reduce storing the values, we sum them up to make them 16bit values.
             __m128i values = _mm_loadu_si128(ptr);
+            __m128i values0 = _mm_srli_epi16(values, 3);
+
             __m128i values1 = _mm_srli_epi16(values, 2);
+            values1 = _mm_or_si128(values0, values1);
             values = _mm_or_si128(values, values1);
             __m128i values2 = _mm_srli_epi16(values, 1);
             values = _mm_or_si128(values, values2);
@@ -218,7 +230,7 @@ static int64_t sCalculateEnergy(const char* data, int hashStart)
         v &= 0xffffu;
         energy += v;
 #else
-        alignas(16) uint8_t storeValues[16] = {};
+        alignas(8) uint16_t storeValues[8] = {};
         _mm_storeu_si128((__m128i*)storeValues, sumValues);
         for(uint16_t v : storeValues)
             energy += v;
