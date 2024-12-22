0c5245afe54e075aca5ac7469c89d5ccb44db2ba
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2024-12-22 18:13:46 +0200

    AOC adding chars shared

---
+++ 2024/cpp/src/chars_shared.h
@@ -0,0 +1,397 @@
+
+#ifndef CHAR_LB
+#define CHAR_LB (0x0A)
+#endif
+
+#ifndef CHAR_ESCAPE
+#define CHAR_ESCAPE (0x1B)
+#endif
+
+#ifndef CHAR_SPACE
+#define CHAR_SPACE (0x20)
+#endif
+
+#ifndef CHAR_EXCLAMATION_MARK
+#define CHAR_EXCLAMATION_MARK (0x21)
+#endif
+
+#ifndef CHAR_DOUBLE_QUOTES
+#define CHAR_DOUBLE_QUOTES (0x22)
+#endif
+
+#ifndef CHAR_NUMBER_SIGN
+#define CHAR_NUMBER_SIGN (0x23)
+#endif
+
+#ifndef CHAR_DOLLAR
+#define CHAR_DOLLAR (0x24)
+#endif
+
+#ifndef CHAR_PERCENT
+#define CHAR_PERCENT (0x25)
+#endif
+
+#ifndef CHAR_AMPERSAND
+#define CHAR_AMPERSAND (0x26)
+#endif
+
+#ifndef CHAR_QUOTE
+#define CHAR_QUOTE (0x27)
+#endif
+
+#ifndef CHAR_OPEN_PAREN
+#define CHAR_OPEN_PAREN (0x28)
+#endif
+
+#ifndef CHAR_CLOSE_PAREN
+#define CHAR_CLOSE_PAREN (0x29)
+#endif
+
+
+#ifndef CHAR_ASTERISK
+#define CHAR_ASTERISK (0x2A)
+#endif
+
+#ifndef CHAR_PLUS
+#define CHAR_PLUS (0x2B)
+#endif
+
+#ifndef CHAR_COMMA
+#define CHAR_COMMA (0x2C)
+#endif
+
+#ifndef CHAR_MINUS
+#define CHAR_MINUS (0x2D)
+#endif
+
+#ifndef CHAR_DOT
+#define CHAR_DOT (0x2E)
+#endif
+
+#ifndef CHAR_SLASH
+#define CHAR_SLASH (0x2F)
+#endif
+
+
+
+#ifndef CHAR_ZERO
+#define CHAR_ZERO (0x30)
+#endif
+
+#ifndef CHAR_ONE
+#define CHAR_ONE (0x31)
+#endif
+
+#ifndef CHAR_TWO
+#define CHAR_TWO (0x32)
+#endif
+
+#ifndef CHAR_THREE
+#define CHAR_THREE (0x33)
+#endif
+
+#ifndef CHAR_FOUR
+#define CHAR_FOUR (0x34)
+#endif
+
+#ifndef CHAR_FIVE
+#define CHAR_FIVE (0x35)
+#endif
+
+#ifndef CHAR_SIX
+#define CHAR_SIX (0x36)
+#endif
+
+#ifndef CHAR_SEVEN
+#define CHAR_SEVEN (0x37)
+#endif
+
+#ifndef CHAR_EIGHT
+#define CHAR_EIGHT (0x38)
+#endif
+
+#ifndef CHAR_NINE
+#define CHAR_NINE (0x39)
+#endif
+
+
+
+
+#ifndef CHAR_COLON
+#define CHAR_COLON (0x3A)
+#endif
+
+#ifndef CHAR_SEMICOLON
+#define CHAR_SEMICOLON (0x3B)
+#endif
+
+#ifndef CHAR_LESSER
+#define CHAR_LESSER (0x3C)
+#endif
+
+#ifndef CHAR_EQUAL
+#define CHAR_EQUAL (0x3D)
+#endif
+
+#ifndef CHAR_GREATER
+#define CHAR_GREATER (0x3E)
+#endif
+
+#ifndef CHAR_QUESTION_MARK
+#define CHAR_QUESTION_MARK (0x3F)
+#endif
+
+#ifndef CHAR_AT_SIGN
+#define CHAR_AT_SIGN (0x40)
+#endif
+
+
+
+
+#ifndef CHAR_A
+#define CHAR_A (0x41)
+#endif
+
+#ifndef CHAR_B
+#define CHAR_B (0x42)
+#endif
+
+#ifndef CHAR_C
+#define CHAR_C (0x43)
+#endif
+
+#ifndef CHAR_D
+#define CHAR_D (0x44)
+#endif
+
+#ifndef CHAR_E
+#define CHAR_E (0x45)
+#endif
+
+#ifndef CHAR_F
+#define CHAR_F (0x46)
+#endif
+
+#ifndef CHAR_G
+#define CHAR_G (0x47)
+#endif
+
+#ifndef CHAR_H
+#define CHAR_H (0x48)
+#endif
+
+#ifndef CHAR_I
+#define CHAR_I (0x49)
+#endif
+
+#ifndef CHAR_J
+#define CHAR_J (0x4A)
+#endif
+
+#ifndef CHAR_K
+#define CHAR_K (0x4B)
+#endif
+
+#ifndef CHAR_L
+#define CHAR_L (0x4C)
+#endif
+
+#ifndef CHAR_M
+#define CHAR_M (0x4D)
+#endif
+
+#ifndef CHAR_N
+#define CHAR_N (0x4E)
+#endif
+
+#ifndef CHAR_O
+#define CHAR_O (0x4F)
+#endif
+
+#ifndef CHAR_P
+#define CHAR_P (0x50)
+#endif
+
+#ifndef CHAR_Q
+#define CHAR_Q (0x51)
+#endif
+
+#ifndef CHAR_R
+#define CHAR_R (0x52)
+#endif
+
+#ifndef CHAR_S
+#define CHAR_S (0x53)
+#endif
+
+#ifndef CHAR_T
+#define CHAR_T (0x54)
+#endif
+
+#ifndef CHAR_U
+#define CHAR_U (0x55)
+#endif
+
+#ifndef CHAR_V
+#define CHAR_V (0x56)
+#endif
+
+#ifndef CHAR_W
+#define CHAR_W (0x57)
+#endif
+
+#ifndef CHAR_X
+#define CHAR_X (0x58)
+#endif
+
+#ifndef CHAR_Y
+#define CHAR_Y (0x59)
+#endif
+
+#ifndef CHAR_Z
+#define CHAR_Z (0x5A)
+#endif
+
+#ifndef CHAR_OPEN_BRACKET
+#define CHAR_OPEN_BRACKET (0x5B)
+#endif
+
+#ifndef CHAR_BACK_SLASH
+#define CHAR_BACK_SLASH (0x5C)
+#endif
+
+#ifndef CHAR_CLOSE_BRACKET
+#define CHAR_CLOSE_BRACKET (0x5D)
+#endif
+
+#ifndef CHAR_HAT
+#define CHAR_HAT (0x5E)
+#endif
+
+#ifndef CHAR_UNDER_SCORE
+#define CHAR_UNDER_SCORE (0x5F)
+#endif
+
+#ifndef CHAR_GRAVE
+#define CHAR_GRAVE (0x60)
+#endif
+
+#ifndef CHAR_a
+#define CHAR_a (0x61)
+#endif
+
+#ifndef CHAR_b
+#define CHAR_b (0x62)
+#endif
+
+#ifndef CHAR_c
+#define CHAR_c (0x63)
+#endif
+
+#ifndef CHAR_d
+#define CHAR_d (0x64)
+#endif
+
+#ifndef CHAR_e
+#define CHAR_e (0x65)
+#endif
+
+#ifndef CHAR_f
+#define CHAR_f (0x66)
+#endif
+
+#ifndef CHAR_g
+#define CHAR_g (0x67)
+#endif
+
+#ifndef CHAR_h
+#define CHAR_h (0x68)
+#endif
+
+#ifndef CHAR_i
+#define CHAR_i (0x69)
+#endif
+
+#ifndef CHAR_j
+#define CHAR_j (0x6A)
+#endif
+
+#ifndef CHAR_k
+#define CHAR_k (0x6B)
+#endif
+
+#ifndef CHAR_l
+#define CHAR_l (0x6C)
+#endif
+
+#ifndef CHAR_m
+#define CHAR_m (0x6D)
+#endif
+
+#ifndef CHAR_n
+#define CHAR_n (0x6E)
+#endif
+
+#ifndef CHAR_o
+#define CHAR_o (0x6F)
+#endif
+
+#ifndef CHAR_p
+#define CHAR_p (0x70)
+#endif
+
+#ifndef CHAR_q
+#define CHAR_q (0x71)
+#endif
+
+#ifndef CHAR_r
+#define CHAR_r (0x72)
+#endif
+
+#ifndef CHAR_s
+#define CHAR_s (0x73)
+#endif
+
+#ifndef CHAR_t
+#define CHAR_t (0x74)
+#endif
+
+#ifndef CHAR_u
+#define CHAR_u (0x75)
+#endif
+
+#ifndef CHAR_v
+#define CHAR_v (0x76)
+#endif
+
+#ifndef CHAR_w
+#define CHAR_w (0x77)
+#endif
+
+#ifndef CHAR_x
+#define CHAR_x (0x78)
+#endif
+
+#ifndef CHAR_y
+#define CHAR_y (0x79)
+#endif
+
+#ifndef CHAR_z
+#define CHAR_z (0x7A)
+#endif
+
+#ifndef CHAR_OPEN_BRACE
+#define CHAR_OPEN_BRACE (0x7B)
+#endif
+
+#ifndef CHAR_VERTICAL_BAR
+#define CHAR_VERTICAL_BAR (0x7C)
+#endif
+
+#ifndef CHAR_CLOSE_BRACE
+#define CHAR_CLOSE_BRACE (0x7D)
+#endif
+
+#ifndef CHAR_TILDE
+#define CHAR_TILDE (0x7E)
+#endif
