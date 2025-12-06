75313384ec3bcd69501ede4d1bbbc4fddd9e9d43
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2025-12-06 12:44:56 +0200

    2025 06 bit cleaner

--- 2025/06/06.py
+++ 2025/06/06.py
@@ -4,25 +4,16 @@ import math
 import re
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
 
-file_path = dir_path + "data.txt"
+file_path = dir_path + "test.txt"
 
 def a():
     sum_of_numbers = 0
-    numbers = []
-    with open(file_path, "r") as file:
-        for line in file:
-            count = 0
-            for i in re.findall(r"[+*]", line):
-                number = 0 if i == '+' else 1
-                for j in numbers[count]:
-                    number = (number + j) if i == '+' else (number * j)
-                sum_of_numbers += number
-                count += 1
-            for i in re.findall(r"\d+", line):
-                if len(numbers) <= count:
-                    numbers.append([])
-                numbers[count].append(int(i))
-                count += 1
+    lines = open(file_path, "r").readlines()
+    opers = re.findall(r"[+*]", lines[-1])
+    numbers = [re.findall(r"\d+", l) for l in lines[0: -1]]
+    for i in range(len(opers)):
+        ns = [int(nums[i]) for nums in numbers]
+        sum_of_numbers += sum(ns) if opers[i] == '+' else math.prod(ns)
 
     print(f"6a - Sum of numbers: {sum_of_numbers}")
 
@@ -30,22 +21,18 @@ def b():
     sum_of_numbers = 0
     lines = open(file_path, "r").readlines()
     max_len = max(list([len(l) for l in lines]))
-    op_chars = [r.span(0)[0] for r in re.finditer(r"[+*]", lines[-1])]
-    # adding last one for ending
-    op_chars.append(max_len + 2)
-    for i in range(len(op_chars) - 1):
-        is_plus = lines[-1][op_chars[i]] == '+'
-        outer_number = 0 if is_plus else 1
-        for char_index in range(op_chars[i], op_chars[i + 1] - 1):
-            number = []
-            for l in lines:
-                if char_index < len(l) and l[char_index].isdigit():
-                    number.append(l[char_index])
-            number = int("".join(number)) if len(number) > 0 else 0
-            outer_number = (outer_number + number) if is_plus else (outer_number * number)
-            #print(f"inner : {number}")
-        #print(f"outer : {outer_number}")
-        sum_of_numbers += outer_number
+
+    numbers = []
+    for char_index in reversed(range(max_len)):
+        number = ""
+        for l in lines[:-1]:
+            c = l[char_index] if char_index < len(l) else ' '
+            number = number + c.strip()
+        if len(number) > 0: numbers.append(int(number))
+        c = lines[-1][char_index] if char_index < len(lines[-1]) else ' '
+        if c == '+' or c == '*':
+            sum_of_numbers += sum(numbers) if c == '+' else math.prod(numbers)
+            numbers.clear()
 
     print(f"6b - Sum of numbers: {sum_of_numbers}")
 
