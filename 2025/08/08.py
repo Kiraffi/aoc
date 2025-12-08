88027ed05f114fcbb9ab0cd44f9efe8b5e7de585
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2025-12-09 00:59:14 +0200

    aoc 2025 day 08 try to reduce lines

--- 2025/08/08.py
+++ 2025/08/08.py
@@ -10,45 +10,32 @@ file_path = dir_path + "data.txt"
 def a():
     junctions = [list(map(int, line.split(','))) for line in open(file_path, "r").readlines()]
     distances = [[j1, j2, math.dist(j1, j2)] for i1, j1 in enumerate(junctions) for j2 in junctions[i1 + 1:]]
-
     distances = sorted(distances, key = lambda d : d[2])
+
     circuits = []
-    for i, d in enumerate(distances[:1000]):
-        inds = [len(circuits), len(circuits)]
+    lam = lambda x: min([i if x in c else len(circuits) - 1 for i, c in enumerate(circuits)])
+    for d in distances[:1000]:
         circuits.append(set([tuple(d[0]), tuple(d[1])]))
-        for i in reversed(range(len(circuits))):
-            if tuple(d[0]) in circuits[i]: inds[0] = i
-            if tuple(d[1]) in circuits[i]: inds[1] = i
+        inds = [lam(tuple(dis)) for dis in d[0:2]]
         if inds[0] != inds[1]:
             circuits[min(inds)].update(circuits[max(inds)])
-            #if max(inds) != len(circuits) - 1:
-            #    del circuits[-1]
-            #del circuits[max(inds)]
-            #circuits.pop(max(inds))
-        #elif inds[0] != len(circuits) - 1: # both ind1 and ind2 are same value
-        #    del circuits[-1]
 
-    circuits = sorted(circuits, key = lambda d : len(d))
-    lens = [len(c) for c in circuits]
-    lens = sorted(list(set(lens)), reverse=True)
+    lens = sorted(list(set([len(c) for c in circuits])))
 
-    print(f"8a - Production of 3 largest circuits: {math.prod(lens[0:3])}")
+    print(f"8a - Production of 3 largest circuits: {math.prod(lens[-3:])}")
 
 def b():
     junctions = [list(map(int, line.split(','))) for line in open(file_path, "r").readlines()]
     distances = [[j1, j2, math.dist(j1, j2)] for i1, j1 in enumerate(junctions) for j2 in junctions[i1 + 1:]]
-
     distances = sorted(distances, key = lambda d : d[2])
 
     connections = set()
-    last_one = []
-    for i, d in enumerate(distances):
+    for d in distances:
         connections.update(set([tuple(d[0]), tuple(d[1])]))
         if len(connections) == len(junctions):
-            last_one = d
             break
 
-    print(f"8b - X coordinates: {last_one[0][0] * last_one[1][0]}")
+    print(f"8b - X coordinates: {d[0][0] * d[1][0]}")
 
 a()
 b()
