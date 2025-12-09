90226276e548101a455f44e76b9014839ba8b8ff
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2025-12-10 00:46:06 +0200

    aoc 2025 day 09, speed up with sorting and make it shorter

--- 2025/09/09.py
+++ 2025/09/09.py
@@ -10,26 +10,23 @@ file_path = dir_path + "data.txt"
 def a():
     corners = [list(map(int, line.split(','))) for line in open(file_path, "r").readlines()]
     rect_v2 = lambda v0, v1 : (abs(v1[0] - v0[0]) + 1) * (abs(v1[1] - v0[1]) + 1)
-    part_1_distance = max([rect_v2(j1, j2) for i1, j1 in enumerate(corners) for j2 in corners[i1 + 1:]])
+    pairs = ((c0, c1) for i1, c0 in enumerate(corners) for c1 in corners[i1 + 1:])
+    part_1_distance = max((rect_v2(c0, c1) for c0, c1 in pairs))
     print(f"9a - Biggest rectangle: {part_1_distance}")
 
 def b():
     corners = [list(map(int, line.split(','))) for line in open(file_path, "r").readlines()]
     rect_v2 = lambda v0, v1 : (abs(v1[0] - v0[0]) + 1) * (abs(v1[1] - v0[1]) + 1)
-    min_v2 = lambda v0, v1 : [min(v0[0], v1[0]), min(v0[1], v1[1])]
-    max_v2 = lambda v0, v1 : [max(v0[0], v1[0]), max(v0[1], v1[1])]
     pairs = ((c0, c1) for i1, c0 in enumerate(corners) for c1 in corners[i1 + 1:])
+    orig_distances = sorted(([rect_v2(c0, c1), c0, c1] for c0, c1 in pairs), key=lambda x:x[0], reverse=True)
 
-    distances = []
-    for j1, j2 in pairs:
-        c0, c1 = [min_v2(j1, j2), max_v2(j1, j2)]
-        if all( (p0[0] <= c0[0] and p1[0] <= c0[0]) or (p0[0] >= c1[0] and p1[0] >= c1[0]) or \
-                (p0[1] <= c0[1] and p1[1] <= c0[1]) or (p0[1] >= c1[1] and p1[1] >= c1[1]) \
-                for p0, p1 in ((p0, corners[i - 1]) for i, p0 in enumerate(corners)) \
-        ):
-            distances.append(rect_v2(j1, j2))
+    outside = lambda p0, p1, e0, e1 : max(p0, p1) <= min(e0, e1) or min(p0, p1) >= max(e0, e1)
+    lines = sorted(((c, corners[i - 1]) for i, c in enumerate(corners)), key=lambda x : rect_v2(x[0], x[1]), reverse=True)
+    for dist, c0, c1 in orig_distances:
+        if all(outside(p0[0], p1[0], c0[0], c1[0]) or outside(p0[1], p1[1], c0[1], c1[1]) for p0, p1 in lines):
+            break
 
-    print(f"9b - Biggest rectangle: {sorted(distances)[-1]}")
+    print(f"9b - Biggest rectangle: {dist}")
 
 a()
 b()
