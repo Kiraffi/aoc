adbeba3e2021554cafd16633428b3052b9a96ace
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2025-12-14 12:45:21 +0200

    aoc 2025 dayv10 more cleaning up

--- 2025/10/10.py
+++ 2025/10/10.py
@@ -34,7 +34,7 @@ def a():
 
 
 eqs = []
-frozen = []
+coeffs = []
 limits = []
 presses = 2**60
 
@@ -47,7 +47,7 @@ def gdc(a, b):
 
 def b():
     global eqs
-    global frozen
+    global coeffs
     global limits
     global presses
     button_presses_total = 0
@@ -73,21 +73,21 @@ def b():
              eqs[j_ind][1] = jolt
              eqs[j_ind][2] = j_ind
 
-        sort_lam = lambda x : sum((1 if not is_zero(y) else 0)*(2**(len(x) - i)) for i, y in enumerate(x[0]))
-
-        eqs = sorted(eqs, key=sort_lam, reverse=True)
-
-        variable_count = len(eqs[0][0])
-
         def find_coeff_max_limits():
-            while True:
-                limits_copy = copy.deepcopy(limits)
+            changed = True
+            while changed:
+                changed = False
+
                 def set_limit(eq):
+                    changed = False
                     for i in range(len(limits)):
-                        if not is_zero(eq[0][i]):
-                            lim_num = min(limits[i], int(math.ceil(eq[1] / eq[0][i])))
-                            if lim_num > 0:
-                                limits[i] = lim_num
+                        if is_zero(eq[0][i]):
+                            continue
+                        lim_num = min(limits[i], int(math.ceil(eq[1] / eq[0][i])))
+                        if lim_num > 0 and limits[i] != lim_num:
+                            limits[i] = lim_num
+                            changed = True
+                    return changed
 
                 for eq in eqs:
                     pos_neg_coeffs = ([x > 0 and not is_zero(x), x < 0 and not is_zero(x)] for x in eq[0])
@@ -104,89 +104,77 @@ def b():
                             if not is_zero(eq[0][i]) and eq[0][i] > 0:
                                 tmp_eq[1] -= eq[0][i] * limits[i]
                                 tmp_eq[0][i] = 0
-                        set_limit(tmp_eq)
+                        changed = set_limit(tmp_eq) or changed
+
                     else:
-                        set_limit(eq)
-                if all(x == y for x, y in zip(limits, limits_copy)):
+                        changed = set_limit(eq) or changed
+                if not changed:
                     break
+
         limits = [10000000000000] * len(eqs[0][0])
         find_coeff_max_limits()
 
-
-
-        def has_only_pivot(r0):
+        def find_new_coeffs(eq):
             count = 0
             found_index = 0
-            for col, coeff in enumerate(r0[0]):
+            for col, coeff in enumerate(eq[0]):
                 if not is_zero(coeff):
                     found_index = col
                 else:
                     count +=1
-            if count != len(r0[0]) - 1:
+            if count != len(eq[0]) - 1:
                 return False
-            if any(f[0] == found_index for f in frozen):
+            if any(coeff[0] == found_index for coeff in coeffs):
                 return False
-            val = (r0[1] // r0[0][found_index])
-            frozen.append([found_index, val])
+            val = (eq[1] // eq[0][found_index])
+            coeffs.append([found_index, val])
             return True
 
         def back_substitute():
-            for f in frozen:
+            for coeff in coeffs:
                 for eq in eqs:
-                    eq[1] -= f[1] * eq[0][f[0]]
-                    eq[0][f[0]] = 0
+                    eq[1] -= coeff[1] * eq[0][coeff[0]]
+                    eq[0][coeff[0]] = 0
             found = False
             for eq in eqs:
-                found = has_only_pivot(eq) or found
+                if find_new_coeffs(eq): found = True
             if found:
                  back_substitute()
 
 
-        frozen = []
+        coeffs = []
         start_time = timer()
 
         def get_pivot_index(r0):
-            for num0 in range(len(r0[0])):
-                if(not is_zero(r0[0][num0])):
+            for num0, row_value in enumerate(r0[0]):
+                if(not is_zero(row_value)):
                     return num0
             return len(r0[0])
 
-        def has_multiple_row_same_pivot():
-            mins = [get_pivot_index(eq) for eq in eqs ]
-            for r0, m0 in enumerate(mins):
-                for r1, m1 in enumerate(mins):
-                    if r1 == r0:
-                        continue
-                    if m0 == m1 and m0 != len(eqs[0][0]):
-                        return True
-            return False
+        def remove_pivot_from_other_rows(eq, from_row_index):
+            num0 = get_pivot_index(eq)
+            if num0 >= len(eq[0]):
+                return
 
-        def remove_pivot_from_other_rows(r0, from_row_index):
-            num0 = get_pivot_index(r0)
-            if num0 < len(r0[0]):
-                for eq2 in eqs[from_row_index:]:
-                    if r0 == eq2:
-                        continue
-                    if not is_zero(eq2[0][num0]):
-                        diff = eq2[0][num0]
-                        g = gdc(abs(r0[0][num0]), abs(diff))
-                        diff = diff // g
-                        multi = r0[0][num0] // g
-                        for rrr in range(len(eq2[0])):
-                            eq2[0][rrr] = (eq2[0][rrr] * multi) - diff * r0[0][rrr]
-                        eq2[1] = eq2[1] * multi - diff * r0[1]
+            for eq2 in eqs[from_row_index:]:
+                if eq == eq2 or is_zero(eq2[0][num0]):
+                    continue
 
+                g = gdc(abs(eq[0][num0]), abs(eq2[0][num0]))
+                diff = eq2[0][num0] // g #current rows coeff, one to be removed
+                multi = eq[0][num0] // g
+                for rrr in range(len(eq2[0])):
+                    eq2[0][rrr] = (eq2[0][rrr] * multi) - diff * eq[0][rrr]
+                eq2[1] = eq2[1] * multi - diff * eq[1]
+
+        # sort the rows in order of leftmost pivots
+        sort_lam = lambda x : sum((1 if not is_zero(y) else 0)*(2**(len(x) - i)) for i, y in enumerate(x[0]))
 
         def gauss_elim():
-            global eqs
-            global frozen
-            frozen_len = -1
-            while frozen_len != len(frozen) or has_multiple_row_same_pivot():
-                frozen_len = len(frozen)
-                for row_index in range(len(eqs)):
-                    eqs.sort(key=sort_lam, reverse=True)
-                    use_row = eqs[row_index]
-                    remove_pivot_from_other_rows(use_row, row_index)
+            for row_index in range(len(eqs)):
+                eqs.sort(key=sort_lam, reverse=True)
+                use_row = eqs[row_index]
+                remove_pivot_from_other_rows(use_row, row_index)
 
         gauss_elim()
 
@@ -211,18 +199,19 @@ def b():
 
         def rec():
             global eqs
-            global frozen
+            global coeffs
             global limits
             global presses
 
-            if any(f[1] < 0 for f in frozen):
+            if any(coeff[1] < 0 for coeff in coeffs):
                 return
-            if sum(f[1] for f in frozen) >= presses:
+            if sum(coeff[1] for coeff in coeffs) >= presses:
                 return
-            if len(frozen) != len(eqs[0][0]):
+            if len(coeffs) != len(eqs[0][0]):
 
                 copy_eqs = copy.deepcopy(eqs)
-                copy_frozen = copy.deepcopy(frozen)
+                copy_coeffs = copy.deepcopy(coeffs)
+                copy_limits = copy.deepcopy(limits)
 
                 find_coeff_max_limits()
 
@@ -231,43 +220,40 @@ def b():
                     for iiii, vvvv in enumerate(eq[0]):
                         most_numbers[iiii] += 1 if not is_zero(vvvv) else 0
 
-                copy_limits = copy.deepcopy(limits)
                 smallest_limits = [[i, x] for i, x in enumerate(limits)]
-
                 most_numbers, smallest_limits = zip(*sorted(zip(most_numbers, smallest_limits), reverse=True))
 
+
                 for limit_value in smallest_limits:
-                    if any(f[0] == limit_value[0] for f in frozen):
+                    if any(coeff[0] == limit_value[0] for coeff in coeffs):
                         continue
                     for limit_range in range(limit_value[1] + 1):
+
                         eqs = copy.deepcopy(copy_eqs)
-                        frozen = copy.deepcopy(copy_frozen)
+                        coeffs = copy.deepcopy(copy_coeffs)
                         limits = copy.deepcopy(copy_limits)
-                        frozen.append([limit_value[0], limit_range])
+
+                        coeffs.append([limit_value[0], limit_range])
                         back_substitute()
                         rec()
 
-            elif len(frozen) == len(eqs[0][0]):
-                values = [0] * len(jolts)
-                for f in frozen:
-                    for b in buttons[f[0]]:
-                        values[b] += f[1]
-                if all(x == jolts[iii] for iii, x in enumerate(values)):
-                    new_sum = sum(x[1] for x in frozen)
+            elif len(coeffs) == len(eqs[0][0]):
+                if  all(all(value == 0 for value in eq[0]) and eq[1] == 0 for eq in eqs):
+                    new_sum = sum(x[1] for x in coeffs)
                     presses = min(presses, new_sum)
             return
 
 
-        values = [0] * len(jolts)
-        for f in frozen:
-            for b in buttons[f[0]]:
-                values[b] += f[1]
-        if all(x == jolts[iii] for iii, x in enumerate(values)):
-            new_sum = sum(x[1] for x in frozen)
+        values = copy.deepcopy(jolts)
+        for coeff in coeffs:
+            for b in buttons[coeff[0]]:
+                values[b] -= coeff[1]
+
+        if all(x == 0 for x in values):
+            new_sum = sum(coeff[1] for coeff in coeffs)
             presses = min(presses, new_sum)
         else:
             rec()
-
         print(f"{line_num + 1}: {presses}")
         button_presses_total += presses
 
