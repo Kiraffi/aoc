36fe1b5b089472982e8dbcb96b98b9594e573841
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2025-12-14 14:36:53 +0200

    aoc 2025 day 10 try to reduce the range by trying to find minimum values

--- 2025/10/10.py
+++ 2025/10/10.py
@@ -63,7 +63,7 @@ def b():
 
 
 
-        #if line_num != 159:
+        #if line_num > 44:
         #    continue
         eqs = [[[0] * len(buttons), 0, 0] for _ in range(len(jolts))]
         for count, b in enumerate(buttons):
@@ -73,19 +73,38 @@ def b():
              eqs[j_ind][1] = jolt
              eqs[j_ind][2] = j_ind
 
-        def find_coeff_max_limits():
+        def find_coeff_limits():
             changed = True
             while changed:
                 changed = False
 
-                def set_limit(eq):
+                def set_max_limits(eq):
+                    changed = False
+                    index = 1 # max limit
+                    for i in range(len(limits)):
+                        if is_zero(eq[0][i]):
+                            continue
+                        lim_num = min(limits[i][index], eq[1] // eq[0][i])
+                        if lim_num >= 0 and limits[i][index] != lim_num:
+                            limits[i][index] = lim_num
+                            changed = True
+                    return changed
+
+                def set_min_limits(eq):
                     changed = False
                     for i in range(len(limits)):
                         if is_zero(eq[0][i]):
                             continue
-                        lim_num = min(limits[i], int(math.ceil(eq[1] / eq[0][i])))
-                        if lim_num > 0 and limits[i] != lim_num:
-                            limits[i] = lim_num
+
+                        r_value = eq[1]
+                        for j in range(len(limits)):
+                            if i == j:
+                                continue
+                            r_value -= eq[0][j] * limits[j][1]
+
+                        value = r_value // eq[0][i]
+                        if(value > limits[i][0] and value <= limits[i][1]):
+                            limits[i][0] = value
                             changed = True
                     return changed
 
@@ -99,20 +118,34 @@ def b():
                         continue
 
                     if has_pos_coeffs and has_neg_coeffs:
-                        tmp_eq = copy.deepcopy(eq)
+                        tmp_max_eq0 = copy.deepcopy(eq)
+                        tmp_max_eq1 = copy.deepcopy(eq)
+                        tmp_min_eq0 = copy.deepcopy(eq)
+                        tmp_min_eq1 = copy.deepcopy(eq)
                         for i in range(len(limits)):
-                            if not is_zero(eq[0][i]) and eq[0][i] > 0:
-                                tmp_eq[1] -= eq[0][i] * limits[i]
-                                tmp_eq[0][i] = 0
-                        changed = set_limit(tmp_eq) or changed
+                            if eq[0][i] > 0:
+                                tmp_max_eq0[1] -= eq[0][i] * limits[i][1]
+                                tmp_min_eq0[1] -= eq[0][i] * limits[i][0]
+                                tmp_max_eq0[0][i] = 0
+                                tmp_min_eq0[0][i] = 0
+                            elif eq[0][i] < 0:
+                                tmp_max_eq1[1] -= eq[0][i] * limits[i][1]
+                                tmp_min_eq1[1] -= eq[0][i] * limits[i][0]
+                                tmp_max_eq1[0][i] = 0
+                                tmp_min_eq1[0][i] = 0
 
+                        if set_max_limits(tmp_max_eq0): changed = True
+                        if set_max_limits(tmp_max_eq1): changed = True
+                        if set_min_limits(tmp_min_eq0): changed = True
+                        if set_min_limits(tmp_min_eq1): changed = True
                     else:
-                        changed = set_limit(eq) or changed
+                        if set_max_limits(eq): changed = True
+                        if set_min_limits(eq): changed = True
                 if not changed:
                     break
 
-        limits = [10000000000000] * len(eqs[0][0])
-        find_coeff_max_limits()
+        limits = [[0, 10000000000000] for _ in eqs[0][0]]
+        find_coeff_limits()
 
         def find_new_coeffs(eq):
             count = 0
@@ -135,10 +168,7 @@ def b():
                 for eq in eqs:
                     eq[1] -= coeff[1] * eq[0][coeff[0]]
                     eq[0][coeff[0]] = 0
-            found = False
-            for eq in eqs:
-                if find_new_coeffs(eq): found = True
-            if found:
+            if any(find_new_coeffs(eq) for eq in eqs):
                  back_substitute()
 
 
@@ -155,11 +185,9 @@ def b():
             num0 = get_pivot_index(eq)
             if num0 >= len(eq[0]):
                 return
-
             for eq2 in eqs[from_row_index:]:
                 if eq == eq2 or is_zero(eq2[0][num0]):
                     continue
-
                 g = gdc(abs(eq[0][num0]), abs(eq2[0][num0]))
                 diff = eq2[0][num0] // g #current rows coeff, one to be removed
                 multi = eq[0][num0] // g
@@ -167,8 +195,9 @@ def b():
                     eq2[0][rrr] = (eq2[0][rrr] * multi) - diff * eq[0][rrr]
                 eq2[1] = eq2[1] * multi - diff * eq[1]
 
+
         # sort the rows in order of leftmost pivots
-        sort_lam = lambda x : sum((1 if not is_zero(y) else 0)*(2**(len(x) - i)) for i, y in enumerate(x[0]))
+        sort_lam = lambda x : sum((1 if not is_zero(y) else 0) * (4**(len(x) - i)) for i, y in enumerate(x[0]))
 
         def gauss_elim():
             for row_index in range(len(eqs)):
@@ -176,26 +205,21 @@ def b():
                 use_row = eqs[row_index]
                 remove_pivot_from_other_rows(use_row, row_index)
 
-        gauss_elim()
-
         def gauss_jordan():
             for eq in reversed(eqs):
                 remove_pivot_from_other_rows(eq, 0)
 
-        #print(f"before gauss_jordan\n{"\n".join(map(str, eqs))}\n")
-        #find_coeff_max_limits()
+        #print(f"before gauss_elim\n{"\n".join(map(str, eqs))}\n")
+        gauss_elim()
 
+        #print(f"before gauss_jordan\n{"\n".join(map(str, eqs))}\n")
         gauss_jordan()
-        #find_coeff_max_limits()
 
         #print(f"after gauss_jordan\n{"\n".join(map(str, eqs))}\n")
-
-        #eqs = gauss_elim(eqs)
         back_substitute()
+
         #print(f"after back_substitute\n{"\n".join(map(str, eqs))}\n")
-
-        #find_coeff_max_limits()
-
+        #find_coeff_limits()
 
         def rec():
             global eqs
@@ -207,18 +231,19 @@ def b():
                 return
             if sum(coeff[1] for coeff in coeffs) >= presses:
                 return
+
             if len(coeffs) != len(eqs[0][0]):
 
                 copy_eqs = copy.deepcopy(eqs)
                 copy_coeffs = copy.deepcopy(coeffs)
                 copy_limits = copy.deepcopy(limits)
 
-                find_coeff_max_limits()
+                find_coeff_limits()
 
                 most_numbers = [0] * len(eqs[0][0])
                 for eq in eqs:
-                    for iiii, vvvv in enumerate(eq[0]):
-                        most_numbers[iiii] += 1 if not is_zero(vvvv) else 0
+                    for eq_index, eq_value in enumerate(eq[0]):
+                        most_numbers[eq_index] += 1 if not is_zero(eq_value) else 0
 
                 smallest_limits = [[i, x] for i, x in enumerate(limits)]
                 most_numbers, smallest_limits = zip(*sorted(zip(most_numbers, smallest_limits), reverse=True))
@@ -227,8 +252,8 @@ def b():
                 for limit_value in smallest_limits:
                     if any(coeff[0] == limit_value[0] for coeff in coeffs):
                         continue
-                    for limit_range in range(limit_value[1] + 1):
 
+                    for limit_range in range(limit_value[1][0], limit_value[1][1] + 1):
                         eqs = copy.deepcopy(copy_eqs)
                         coeffs = copy.deepcopy(copy_coeffs)
                         limits = copy.deepcopy(copy_limits)
@@ -237,10 +262,9 @@ def b():
                         back_substitute()
                         rec()
 
-            elif len(coeffs) == len(eqs[0][0]):
-                if  all(all(value == 0 for value in eq[0]) and eq[1] == 0 for eq in eqs):
-                    new_sum = sum(x[1] for x in coeffs)
-                    presses = min(presses, new_sum)
+            elif all(eq[1] == 0 and all(value == 0 for value in eq[0]) for eq in eqs):
+                new_sum = sum(x[1] for x in coeffs)
+                presses = min(presses, new_sum)
             return
 
 
@@ -254,7 +278,7 @@ def b():
             presses = min(presses, new_sum)
         else:
             rec()
-        print(f"{line_num + 1}: {presses}")
+        print(f"{line_num + 1}: {presses}")# time: {timer() - start_time}")
         button_presses_total += presses
 
     print(f"10b - Button presses: {button_presses_total} run time: {timer() - total_start_time}")
